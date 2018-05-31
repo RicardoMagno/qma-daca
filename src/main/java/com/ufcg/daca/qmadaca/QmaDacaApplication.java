6c934a02c68a57ddb6ca76489cb72a354f135a42
unknown <magnoricardo7@gmail.com>
2018-05-31 14:46:00 -0300

    Daca project

---
+++ src/main/java/com/ufcg/daca/qmadaca/QmaDacaApplication.java
@@ -0,0 +1,12 @@
+package com.ufcg.daca.qmadaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmaDacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmaDacaApplication.class, args);
+	}
+}
